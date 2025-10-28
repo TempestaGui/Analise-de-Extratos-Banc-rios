90e5431b4cffc068f5ce4693a2c92de5d673daf2
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-28 20:50:52 -0300

    BankTransactionalAnalyzerSimple 2.1

---
+++ src/main/java/org/aplicacao/BankTransactionalAnalyzerSimple.java
@@ -0,0 +1,23 @@
+package org.aplicacao;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionalAnalyzerSimple {
+    private static final String RESOURCE = "src/main/resources/extratodBancariosMark.csv";
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(RESOURCE);
+        List<String> lines = Files.readAllLines(path);
+        double total = 0;
+        for(String line : lines){
+            final String[] columns = line.split(",");
+            double amount  = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+        System.out.println("The total for all transactions is: "+total);
+
+    }
+}
