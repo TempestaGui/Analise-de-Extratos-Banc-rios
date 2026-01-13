aab660653fd78a741de526de9bf89963af37c9b8
tempestaGui <guilhermeftempesta@gmail.com>
2026-01-13 19:08:47 -0300

    implementing informational cohesion

---
+++ src/main/java/org/aplicacao/BankTransactionalAnalyzer/DAO/BankTransactionDAO.java
@@ -0,0 +1,27 @@
+package org.aplicacao.BankTransactionalAnalyzer.DAO;
+
+import java.time.LocalDate;
+
+public class BankTransactionDAO {
+
+    public BankTransactionDAO create(final LocalDate date, final double amount, final String description){
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransactionDAO read(final long id){
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransactionDAO update(final long id){
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public void delete(final long id){
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+}
