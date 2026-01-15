301397e09a7f9f1a25379bf4cd4bf5012f5acf80
tempestaGui <guilhermeftempesta@gmail.com>
2026-01-15 20:00:45 -0300

    test: add unity tests

---
+++ src/test/java/utils/BankStatementCSVParserTest.java
@@ -0,0 +1,17 @@
+package utils;
+
+import org.aplicacao.BankTransactionalAnalyzer.utils.BankStatementCSVParser;
+import org.aplicacao.BankTransactionalAnalyzer.utils.BankStatementParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+
+}
