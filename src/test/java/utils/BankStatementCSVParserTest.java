4f57950eb236a22819be7f5cbedbf7e194cfa7ac
tempestaGui <guilhermeftempesta@gmail.com>
2026-01-16 19:43:08 -0300

    test: add shouldParseOneCorrectLine test

--- src/test/java/utils/BankStatementCSVParserTest.java
+++ src/test/java/utils/BankStatementCSVParserTest.java
@@ -1,9 +1,13 @@
 package utils;
 
+import org.aplicacao.BankTransactionalAnalyzer.entities.BankTransactional;
 import org.aplicacao.BankTransactionalAnalyzer.utils.BankStatementCSVParser;
 import org.aplicacao.BankTransactionalAnalyzer.utils.BankStatementParser;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 public class BankStatementCSVParserTest {
 
@@ -11,7 +15,20 @@ public class BankStatementCSVParserTest {
 
     @Test
     public void shouldParseOneCorrectLine() throws Exception {
-        Assert.fail("Not yet implemented");
+        //Given
+        final String line  = "30-01-2017,-50,Tesco";
+
+        //When
+        final BankTransactional result = statementParser.parseFrom(line);
+
+        final BankTransactional expected =
+                    new BankTransactional(-50.0,"Tesco",LocalDate.of(2017, Month.JANUARY, 30));
+        final double tolerance = 0.0d;
+
+        //Then
+        Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assert.assertEquals(expected.getDescription(), result.getDescription());
     }
 
 }
