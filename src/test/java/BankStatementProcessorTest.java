d3311edc0adfab63efbf851344e2ef34e86b6ec3
tempestaGui <guilhermeftempesta@gmail.com>
2026-01-18 18:37:27 -0300

    test: add tests for transactions filtering by amount and month

--- src/test/java/BankStatementProcessorTest.java
+++ src/test/java/BankStatementProcessorTest.java
@@ -62,4 +62,41 @@ public class BankStatementProcessorTest {
         //Then
         assertEquals(150.0, total);
     }
+
+    @Test
+    public void shouldReturnTransactionsGreaterThan() throws Exception {
+        //Given
+        final double amount = 70.0;
+
+        BankTransactional t1 = new BankTransactional(
+                50.0, "teste", LocalDate.of(2017, Month.JANUARY, 30));
+        BankTransactional t2 = new BankTransactional(
+                100.0, "teste", LocalDate.of(2017, Month.JANUARY, 30));
+
+        processor = new BankStatementProcessor(List.of(t1,t2));
+
+        //When
+        List<BankTransactional> result = processor.findTransactionsGreaterThanEqual(amount);
+
+        //Then
+        assertEquals(1, result.size());
+        assertEquals(100, result.getFirst().getAmount());
+    }
+
+    @Test
+    public void shouldReturnTransactionsByMonth() throws Exception {
+        //Given
+        BankTransactional t1 = new BankTransactional(
+                50.0, "teste", LocalDate.of(2017, Month.JANUARY, 30));
+        BankTransactional t2 = new BankTransactional(
+                100.0, "teste", LocalDate.of(2017, Month.JANUARY, 30));
+
+        processor = new BankStatementProcessor(List.of(t1,t2));
+
+        //When
+        List<BankTransactional> result = processor.findTransactionsInMonth(Month.JANUARY);
+
+        //Then
+        assertEquals(2, result.size());
+    }
 }
